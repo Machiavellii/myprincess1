e1aeb8d52eae3a4ab677d23e28b3ded968641eb9
Machiavellii <vjekoslav.seferovic@gmail.com>
2019-10-10 16:10:54 +0200

    backend start point

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+//Database
+//connectDB()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.use('/api/auth', require('./routes/api/auth'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`));
