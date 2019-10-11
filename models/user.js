c053d3a03438f6bd809e642cda354c06ef82e6ed
Atisnet <info@atisnet.com>
2019-10-11 11:59:44 +0200

    remodeled

--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
-const {ObjectId} = mongoose.Schema;
 
 const userSchema = new mongoose.Schema({
+    /* USER remodeled start */
+    
     nickname: {
         type: String,
         required: true
@@ -11,48 +12,7 @@ const userSchema = new mongoose.Schema({
         trim: true,
         required: true
     },
-    gender: {
-        type: String,
-        required: true
-    },
-    sexual_orientation: {
-        type: String,
-        required: true
-    },
-    phone: {
-        type: String,
-        trim: true,
-        required: true,
-    },
-    hashed_password: {
-        type: String,
-        required: true
-    },
-    type: {
-        type: String,
-        required: true
-    },
-    in_agency: {
-        type: Boolean,
-        default: false,
-    },
-    parent_acc: {
-        type: ObjectId,
-        ref: 'userSchema'
-    },
-    country: {
-        type: String,
-        required: true
-    },
-    canton: {
-        type: String,
-        required: true
-    },
-    city: {
-        type: String,
-        required: true
-    },
-    ZIP: {
+    password: {
         type: String,
         required: true
     },
@@ -60,27 +20,23 @@ const userSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     },
-    subscription_plan: {
-        type: ObjectId,
-        ref: 'Subscription'
-    },
-    start_of_subscription: {
-        type: Date
-    },
-    end_of_subscription: {
-        type: Date
-    },
-    favorites: {
-        type: Array,
-    },
-    is_active: {
+
+    /* USER user remodeled end */
+
+   
+     /* removed for now :)
+     
+     in_agency: {
         type: Boolean,
-        default: false
+        default: false,
     },
-    adCount: {
+    parent_acc: {
+        type: ObjectId,
+        ref: 'userSchema'
+    },
+        adCount: {
         type: Number
-    },
-    salt: String
+    } */
 });
 
 module.exports = mongoose.model("User", userSchema);
