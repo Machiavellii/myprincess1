fd88fcfcff8ea8eb83ce198e4179d4f7db51083e
Machiavellii <vjekoslav.seferovic@gmail.com>
2019-10-11 14:52:14 +0200

    .

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -2,8 +2,30 @@ import React from 'react';
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="container">
+      <form className="p-5">
+        <div className="form-group">
+          <label htmlFor="email">Email Address</label>
+          <input
+            type="text"
+            className="form-control"
+            name="email"
+            placeholder="Enter Email"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="text"
+            className="form-control"
+            name="password"
+            placeholder="Enter Password"
+          />
+        </div>
+        <button type="submit" class="btn btn-form">
+          To log in
+        </button>
+      </form>
     </div>
   );
 };
