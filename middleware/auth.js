c0f7ca3736f3683fe3bd36fc54b29c655708d629
Atisnet <info@atisnet.com>
2019-10-15 09:45:30 +0200

    profile issues

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 
 module.exports = function(req, res, next) {
-  const token = req.header('x-auth-token');
+  const token = req.header('Authorization');
 
   //Check if not token
   if (!token) {
