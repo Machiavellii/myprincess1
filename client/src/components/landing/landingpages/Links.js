ac576405c9056451e3a3ba2ca1dbda0dfbf8c695
Machiavellii <vjekoslav.seferovic@gmail.com>
2019-10-16 11:31:35 +0200

    .

--- client/src/components/landing/landingpages/Links.js
+++ client/src/components/landing/landingpages/Links.js
@@ -1,26 +1,101 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Links = () => {
   return (
     <div className="container">
       <h1 className="text-center">Links</h1>
       <div className="holder-links">
-        <a href="http://www.sexjoy.ch/">http://www.sexjoy.ch/</a>
-        <a href="https://lapine.ch/">https://lapine.ch/</a>
-        <a href="https://ubergirls.ch/">https://ubergirls.ch/</a>
-        <a href="http://www.sex4u.ch/">http://www.sex4u.ch/</a>
-        <a href="https://xdiva.ch">https://xdiva.ch</a>
-        <a href="https://www.anibis.ch/">https://www.anibis.ch/</a>
-        <a href="https://www.videogirl.ch/">https://www.videogirl.ch/</a>
-        <a href="https://nikky.ch/">https://nikky.ch/</a>
-        <a href="https://www.sexup.ch/">https://www.sexup.ch/</a>
-        <a href="https://www.bemygirl.ch/">https://www.bemygirl.ch/</a>
-        <a href="https://agence-elegance.ch/">https://agence-elegance.ch/</a>
-        <a href="http://newseduction.ch/">http://newseduction.ch/</a>
-        <a href="https://www.divageneva.ch/">https://www.divageneva.ch/</a>
-        <a href="http://geneva-girls.ch/">http://geneva-girls.ch/</a>
-        <a href="https://www.paradisegeneve.ch/">
+        <a
+          href="http://www.sexjoy.ch/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          http://www.sexjoy.ch/
+        </a>
+        <a href="https://lapine.ch/" target="_blank" rel="noopener noreferrer">
+          https://lapine.ch/
+        </a>
+        <a
+          href="https://ubergirls.ch/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          https://ubergirls.ch/
+        </a>
+        <a
+          href="http://www.sex4u.ch/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          http://www.sex4u.ch/
+        </a>
+        <a href="https://xdiva.ch" target="_blank" rel="noopener noreferrer">
+          https://xdiva.ch
+        </a>
+        <a
+          href="https://www.anibis.ch/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          https://www.anibis.ch/
+        </a>
+        <a
+          href="https://www.videogirl.ch/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          https://www.videogirl.ch/
+        </a>
+        <a href="https://nikky.ch/" target="_blank" rel="noopener noreferrer">
+          https://nikky.ch/
+        </a>
+        <a
+          href="https://www.sexup.ch/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          https://www.sexup.ch/
+        </a>
+        <a
+          href="https://www.bemygirl.ch/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          https://www.bemygirl.ch/
+        </a>
+        <a
+          href="https://agence-elegance.ch/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          https://agence-elegance.ch/
+        </a>
+        <a
+          href="http://newseduction.ch/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          http://newseduction.ch/
+        </a>
+        <a
+          href="https://www.divageneva.ch/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          https://www.divageneva.ch/
+        </a>
+        <a
+          href="http://geneva-girls.ch/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          http://geneva-girls.ch/
+        </a>
+        <a
+          href="https://www.paradisegeneve.ch/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           https://www.paradisegeneve.ch/
         </a>
         <a href="https://diamondmodels.ch/">https://diamondmodels.ch/</a>
