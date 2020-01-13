bf6f079f5c4b05216f7fb9c8a1fa14ad1c79fc9a
Machiavellii <vjekoslav.seferovic@gmail.com>
2020-01-13 17:21:34 +0100

    .

--- client/src/components/landing/landingpages/landingcontent/singleGirl/gallery/GalleryHolder.js
+++ client/src/components/landing/landingpages/landingcontent/singleGirl/gallery/GalleryHolder.js
@@ -1,62 +1,64 @@
-import React, { useState, Fragment } from 'react';
-import Describe from './galleryComponent/Describe';
-import ReactMapGl, { Marker } from 'react-map-gl';
+import React, { useState, Fragment } from "react";
+import Describe from "./galleryComponent/Describe";
+import ReactMapGl, { Marker } from "react-map-gl";
 // import Gallery from './galleryComponent/Gallery';
 
 const GalleryHolder = ({ profile }) => {
-	const { cover_photo, is_active, city, location } = profile;
-	const [viewPort, setViewPort] = useState({
-		latitude: profile.location.coordinates[1],
-		longitude: profile.location.coordinates[0],
-		width: '100%',
-		height: '100%',
-		zoom: 12
-	});
+  const { cover_photo, is_active, city, location } = profile;
+  const [viewPort, setViewPort] = useState({
+    latitude: profile.location.coordinates[1],
+    longitude: profile.location.coordinates[0],
+    width: "100%",
+    height: "100%",
+    zoom: 12
+  });
 
-	console.log(location.formattedAddress);
+  console.log(location.formattedAddress);
 
-	return (
-		<div className='gallery-holder'>
-			<div className='img-holder mb-4'>
-				<img
-					src={`${window.location.origin}/${cover_photo}`}
-					alt=''
-					className='img-fluid'
-				/>
-			</div>
-			<div className='location mb-5'>
-				<h6>Job Activity</h6>
-				<p>{is_active ? 'Active' : 'Inactive'}</p>
-				<p>{location.formattedAddress}</p>
-				<div className='map mb-4'>
-					<ReactMapGl
-						{...viewPort}
-						mapboxApiAccessToken={
-							'pk.eyJ1IjoibWF0ZWpnZWxqaSIsImEiOiJjazU4MjFubTEwNnB1M2xwZnBmb3F3aDI2In0.Q3ao_KXEg2Hr6ziv1Ddo3g'
-						}
-						onViewportChange={viewPort => {
-							setViewPort(viewPort);
-						}}>
-						{!profile ? (
-							<Fragment>Loading...</Fragment>
-						) : (
-							<Marker
-								latitude={profile.location.coordinates[1]}
-								longitude={profile.location.coordinates[0]}>
-								<button className='girl-button-marker'>
-									<i className='fas fa-map-marker-alt girl-button-marker-icon'></i>
-								</button>
-							</Marker>
-						)}
-					</ReactMapGl>
-				</div>
-				<p>Rue Uttins 20VD CH</p>
-				<a href='!#'>Get directions</a>
-			</div>
-			<Describe profile={profile} />
-			{/* <Gallery profile={profile} /> */}
-		</div>
-	);
+  return (
+    <div className="gallery-holder">
+      <div className="img-holder mb-4">
+        <img
+          src={`${window.location.origin}/${cover_photo}`}
+          alt=""
+          className="img-fluid"
+        />
+      </div>
+      <div className="location mb-5">
+        <h6>Job Activity</h6>
+        <p>{is_active ? "Active" : "Inactive"}</p>
+
+        <div className="map mb-4">
+          <ReactMapGl
+            {...viewPort}
+            mapboxApiAccessToken={
+              "pk.eyJ1IjoibWF0ZWpnZWxqaSIsImEiOiJjazU4MjFubTEwNnB1M2xwZnBmb3F3aDI2In0.Q3ao_KXEg2Hr6ziv1Ddo3g"
+            }
+            onViewportChange={viewPort => {
+              setViewPort(viewPort);
+            }}
+          >
+            {!profile ? (
+              <Fragment>Loading...</Fragment>
+            ) : (
+              <Marker
+                latitude={profile.location.coordinates[1]}
+                longitude={profile.location.coordinates[0]}
+              >
+                <button className="girl-button-marker">
+                  <i className="fas fa-map-marker-alt girl-button-marker-icon"></i>
+                </button>
+              </Marker>
+            )}
+          </ReactMapGl>
+        </div>
+        <p>{location.formattedAddress}</p>
+        <a href="!#">Get directions</a>
+      </div>
+      <Describe profile={profile} />
+      {/* <Gallery profile={profile} /> */}
+    </div>
+  );
 };
 
 export default GalleryHolder;
