5151cadb81e39730dab2137e701d74ec377faa9d
Bane96 <bane_dragic96@hotmail.com>
2020-01-13 16:21:21 +0100

    map and marker update

--- client/src/components/pages/search/Search.js
+++ client/src/components/pages/search/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { getProfiles } from '../../../actions/profile';
 
@@ -9,52 +9,49 @@ import Girls from './Girls';
 import FilterForm from './FilterForm';
 
 const Search = ({ profiles, getProfiles }) => {
-  useEffect(() => {
-    getProfiles();
-  }, [getProfiles]);
+	useEffect(() => {
+		getProfiles();
+	}, [getProfiles]);
 
-  let [activeFilter, setActive] = useState(false);
-  let [activeMap, setActiveMap] = useState(false);
+	let [activeFilter, setActive] = useState(false);
+	let [activeMap, setActiveMap] = useState(false);
 
-  const toggleFilter = e => {
-    setActive((activeFilter = !activeFilter));
-  };
+	const toggleFilter = e => {
+		setActive((activeFilter = !activeFilter));
+	};
 
-  const toggleMap = e => {
-    setActiveMap((activeMap = !activeMap));
-  };
+	const toggleMap = e => {
+		setActiveMap((activeMap = !activeMap));
+	};
 
-  return (
-    <div className="row py-1 search">
-      <div className="col-sm-12 d-lg-none btns">
-        <button className="btn btn-rose-back" onClick={toggleFilter}>
-          <i className="fas fa-filter" /> Filter ads
-        </button>
-        <button className="btn" onClick={toggleMap}>
-          View map <i className="far fa-map" />
-        </button>
-      </div>
+	return (
+		<div className='row py-1 search'>
+			<div className='col-sm-12 d-lg-none btns'>
+				<button className='btn btn-rose-back' onClick={toggleFilter}>
+					<i className='fas fa-filter' /> Filter ads
+				</button>
+				<button className='btn' onClick={toggleMap}>
+					View map <i className='far fa-map' />
+				</button>
+			</div>
 
-      <div className="col-md-12 col-lg-6">
-        <div className={activeFilter ? 'd-block' : 'd-none d-lg-block'}>
-          <FilterForm profiles={profiles} />
-        </div>
-        <div className={activeMap ? 'd-none' : 'col-md-12'}>
-          <Girls profiles={profiles} />
-        </div>
-      </div>
-      <div className={activeMap ? 'col-md-12 d-md-block' : 'd-none d-lg-block'}>
-        <Map />
-      </div>
-    </div>
-  );
+			<div className='col-md-12 col-lg-6'>
+				<div className={activeFilter ? 'd-block' : 'd-none d-lg-block'}>
+					<FilterForm profiles={profiles} />
+				</div>
+				<div className={activeMap ? 'd-none' : 'col-md-12'}>
+					<Girls profiles={profiles} />
+				</div>
+			</div>
+			<div className={activeMap ? 'col-md-12 d-md-block' : 'd-none d-lg-block'}>
+				<Map />
+			</div>
+		</div>
+	);
 };
 
 const mapStateToProps = state => ({
-  profiles: state.profile
+	profiles: state.profile
 });
 
-export default connect(
-  mapStateToProps,
-  { getProfiles }
-)(Search);
+export default connect(mapStateToProps, { getProfiles })(Search);
