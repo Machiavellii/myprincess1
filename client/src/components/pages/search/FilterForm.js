1976584b939df0dd0cb2c9f37f5a9432efb17055
Bane96 <bane_dragic96@hotmail.com>
2020-01-17 15:27:52 +0100

    multiselect form control

--- client/src/components/pages/search/FilterForm.js
+++ client/src/components/pages/search/FilterForm.js
@@ -10,7 +10,7 @@ import { filterSearchPage } from '../../../actions/profile';
 const FilterForm = ({ filterSearchPage }) => {
 	const [formData, setFormData] = useState({
 		canton: '',
-		services: '',
+		services: [],
 		category: ''
 	});
 
@@ -23,6 +23,19 @@ const FilterForm = ({ filterSearchPage }) => {
 		// setFormData(e.target.value);
 		// filterSearchPage(e.target.value);
 	};
+
+	const onCheckBoxServ = (e, service) => {
+		if (services.indexOf(e.target.value) < 1 && e.target.checked) {
+			services.push(service);
+		}
+
+		services.map((serv, i) => {
+			if (!e.target.checked) {
+				return e.target.value === serv ? services.splice(i, 1) : services;
+			}
+		});
+	};
+
 	const { canton, category, services } = formData;
 
 	return (
@@ -55,20 +68,42 @@ const FilterForm = ({ filterSearchPage }) => {
 					);
 				})}
 			</select>
-			<select
-				className='form-control'
-				name='services'
-				onChange={e => onChange(e)}
-				value={services}>
-				<option value='0'> - Prestations - </option>
-				{servicesList.map((item, index) => {
-					return (
-						<option key={index} value={item}>
-							{item}
-						</option>
-					);
-				})}
-			</select>
+			<div className='dropdown'>
+				<a
+					className='nav-link dropdown-toggle form-control'
+					href='#'
+					role='button'
+					id='dropdownMenuLink'
+					data-toggle='dropdown'
+					aria-haspopup='true'
+					aria-expanded='false'>
+					- Chooce wanted services -
+				</a>
+				<div
+					className='dropdown-menu scrollable-menu'
+					aria-labelledby='dropdownMenuButton'
+					style={{ backgroundColor: 'white' }}>
+					{servicesList.map((service, index) => {
+						return (
+							<li className='nav-item'>
+								<input
+									className='form-check-input ml-1'
+									type='checkbox'
+									id={service}
+									value={service}
+									name='services'
+									onChange={e => onCheckBoxServ(e, service)}
+								/>
+								<label
+									className='form-check-label dynamic-checkbox-label ml-4'
+									htmlFor={service}>
+									{service}
+								</label>
+							</li>
+						);
+					})}
+				</div>
+			</div>
 		</div>
 	);
 };
