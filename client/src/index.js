bb4e5eb711c1d61889be52198acd38cf458c8298
Bane96 <bane_dragic96@hotmail.com>
2020-02-18 15:07:23 +0100

    pwa-fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,16 @@
-import React, { Suspense } from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./i18next";
-import Spinner from './components/layout/Spinner'
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './i18next';
+import Spinner from './components/layout/Spinner';
+
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Suspense fallback={<Spinner />}>
-    <App />
-  </Suspense>,
-  document.getElementById("root")
+	<Suspense fallback={<Spinner />}>
+		<App />
+	</Suspense>,
+	document.getElementById('root')
 );
+
+serviceWorker.register();
