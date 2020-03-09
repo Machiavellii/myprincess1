80e255109612f89211bd0498e1fd0d82910a79d0
Machiavellii <vjekoslav.seferovic@gmail.com>
2020-03-09 09:16:16 +0100

    .

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
+const cors = require("cors");
 
 // Prevent attacks
-const mongoSanitize = require('express-mongo-sanitize');
-const helmet = require('helmet');
-const xss = require('xss-clean');
-const rateLimit = require('express-rate-limit');
-const hpp = require('hpp');
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
 
 const app = express();
 
@@ -34,35 +34,35 @@ app.use(xss());
 
 // Set express Rate Limiting
 const limiter = rateLimit({
-	windowMs: 10 * 60 * 1000, // 10 minutes
-	max: 100
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100
 });
 app.use(limiter);
 
 //Prevent HPP param polution
-//app.use(hpp());
+app.use(hpp());
 
 // END PROTECT
 
 // Routes
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/profile', require('./routes/api/profiles'));
-app.use('/api/agency', require('./routes/api/agencyProfile'));
-app.use('/api/admin', require('./routes/api/admin'));
-app.use('/api/opinion', require('./routes/api/opinion'));
-app.use('/api/payment', require('./routes/api/payment'));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profiles"));
+app.use("/api/agency", require("./routes/api/agencyProfile"));
+app.use("/api/admin", require("./routes/api/admin"));
+app.use("/api/opinion", require("./routes/api/opinion"));
+app.use("/api/payment", require("./routes/api/payment"));
 
-app.use('/static/images', express.static('static/images'));
+app.use("/static/images", express.static("static/images"));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-	// Set static folder
-	app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
