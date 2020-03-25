edf40334b138a5d0826c8aed3e9993ec16e9f72d
Machiavellii <vjekoslav.seferovic@gmail.com>
2020-03-25 15:32:38 +0100

    .

--- client/src/components/landing/landingpages/landingcontent/singleGirl/describeGirl/componentGirls/HeaderGirl.js
+++ client/src/components/landing/landingpages/landingcontent/singleGirl/describeGirl/componentGirls/HeaderGirl.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React from "react";
 
 const HeaderGirl = ({ profile }) => {
   const {
     location,
     user: { nickname },
     phone,
-    slogan
+    slogan,
+    webcamlink
   } = profile;
+
+  console.log(profile);
 
   return (
     <div className="headerGirl">
       <div className="container">
         <h1>{nickname}</h1>
         <p className="subheading">{slogan}</p>
-        <a href={`tel:${phone}`}>
+        <a href={`tel:${phone}`} className="mb-3 d-block">
           <i className="fas fa-phone rotate" /> {phone}
         </a>
+
+        {webcamlink ? (
+          <a href={webcamlink} style={{ cursor: "pointer" }}>
+            <i class="fas fa-video"></i> Webcam link
+          </a>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
